b84be4848d61309357cf2657848cce1c6b6afe22
AmonisLG <ledzhinov@gmail.com>
2022-12-08 12:25:43 +0300

    Добавлен ответ на задачу номер 8

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите любое трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 100 && a < 1000)
+{
+    int n1 = a % 100;
+    int n2 = n1 / 10;
+    Console.WriteLine($"Вторая цифра этого числа: {n2}");
+}
+else
+    Console.WriteLine("Введено не верное число!");
